d7fe1fe88688ff66743329d31fe6eb9d30780a26
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-11-01 13:58:51 +0500

    Update EmployeeRepo.java

--- Elastic-Search-App/src/main/java/com/jk/elasticsearch/repo/EmployeeRepo.java
+++ Elastic-Search-App/src/main/java/com/jk/elasticsearch/repo/EmployeeRepo.java
@@ -5,6 +5,11 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
 import java.util.List;
 
+**
+ * @author Junaid.Khan
+ */
+
+
 public interface EmployeeRepo extends ElasticsearchRepository<Employee,String> {
     List<Employee> findByFirstName(String firstName);
 }
